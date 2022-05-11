b58df3f972eddf16bf07c4e1069c20e4cc90a7a1
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-10 23:17:49 -0300

    :pencil2: First commit to Alura studies w/ Next.js

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link'
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>Página não encontrada!!</h1>
+      <Link href="/">Voltar para Home</Link>
+    </div>
+  )
+}
---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function faq() {
+  return (
+    <div>
+      <h1>Alura Case - Página Faq</h1>
+      <Link href="/">
+        <a>Ir para Home</a>
+      </Link>
+    </div>
+  )
+}
